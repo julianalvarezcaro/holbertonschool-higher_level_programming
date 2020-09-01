51ad2754f94b67823284071ef2b2ac584c261035
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-09-01 23:44:21 +0000

    COrrected pep8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,8 @@ def uppercase(str):
     for pos in range(len(str)):
         aski = ord(str[pos])
         if aski > 96 and aski < 123:
-            print("{}".format(chr(aski - 32)), end='')
+            upaski = chr(askii - 32)
         else:
-            print("{}".format(chr(aski)), end='')
+            upaski = chr(askii)
+        print("{}".format(upaski), end='')
     print("")
