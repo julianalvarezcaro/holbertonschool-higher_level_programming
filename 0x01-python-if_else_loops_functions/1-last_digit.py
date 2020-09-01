5848010e98be6b476217d82b50b46bc89bd8c3bb
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-09-01 20:59:46 +0000

    Corrected last condition and deleted unnecesary and lastdig != 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,6 +10,6 @@ if lastdig > 5:
  is greater than 5".format(number, lastdig))
 elif lastdig == 0:
     print("Last digit of {} is {} and is 0".format(number, lastdig))
-elif lastdig < 6 and lastdig != 0:
+elif lastdig < 6:
     print("Last digit of {} is {} and\
  is less than 6 and not 0".format(number, lastdig))
