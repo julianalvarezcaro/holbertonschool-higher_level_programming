69db056e55928b5c92edc11aabff6ee5087600b8
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-09-03 21:02:11 +0000

    Completed task 9

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+from iwasinalpha import alpha
+alpha()
---
+++ 0x02-python-import_modules/iwasinalpha.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def alpha():
+    for letter in range(65, 91):
+        print("{}".format(chr(letter)), end='')
+    print()
