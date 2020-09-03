aa96d5a5d451aa256f23103b5dd4a64837b61672
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-09-03 17:27:06 +0000

    Completed task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    args = sys.argv #I save the args from the command line to this variable
+    narg = len(args) #I get the len of that list. AKA # of elements
+    if narg == 1:
+        sep = "."
+    else:
+        sep = ": "
+    if narg == 2:
+        argstr = "argument"
+    else:
+        argstr = "arguments"
+    print("{} {}{}".format(narg - 1, argstr, sep))
+    for pos in range(1, narg):
+        print("{}{}{}".format(pos, sep, args[pos]))
