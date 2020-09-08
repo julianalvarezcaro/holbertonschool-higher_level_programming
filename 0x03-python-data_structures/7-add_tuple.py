650947eac091a944d495cc7ca26618a160922191
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-09-08 22:40:14 +0000

    Instead of using that mess of conditionals, used one the cute abstraction of tuples c:

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0:
-        tuple_ca = (0, 0)
-    elif len(tuple_a) == 1:
-        tuple_ca == (tuple_a[0], 0)
-    else:
-        tuple_ca = tuple_a
-    if len(tuple_b) == 0:
-        tuple_cb = (0, 0)
-    elif len(tuple_b) == 1:
-        tuple_cb = (tuple_b[0], 0)
-    else:
-        tuple_cb = tuple_b
+    tuple_ca = tuple_a + (0, 0)
+    tuple_cb = tuple_b + (0, 0)
     tuple_res = (tuple_ca[0] + tuple_cb[0], tuple_ca[1] + tuple_cb[1])
     return tuple_res
