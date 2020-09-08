9d795690e5952be7a3e4a1c3fedd8334a5e6d75f
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-09-08 22:16:47 +0000

    Completed task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/pyhon3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) == 0:
+        tuple_ca = (0, 0)
+    elif len(tuple_a) == 1:
+        tuple_ca == (tuple_a[0], 0)
+    else:
+        tuple_ca = tuple_a
+    if len(tuple_b) == 0:
+        tuple_cb = (0, 0)
+    elif len(tuple_b) == 1:
+        tuple_cb = (tuple_b[0], 0)
+    else:
+        tuple_cb = tuple_b
+    tuple_res = (tuple_ca[0] + tuple_cb[0], tuple_ca[1] + tuple_cb[1])
+    return tuple_res
