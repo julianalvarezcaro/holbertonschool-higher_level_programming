e027217c6476c0cfc137397af8540f61d135d2ea
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-09-08 18:08:42 +0000

    Trying a different format

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
     for pos in range(len(my_list)):
-        print("{:d}".format(my_list[pos]))
+        print("{:n}".format(my_list[pos]))
