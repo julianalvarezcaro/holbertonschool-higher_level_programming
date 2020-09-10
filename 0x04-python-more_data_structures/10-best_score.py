eda5187d8a9394e2230a00d84f01d7bc92989867
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-09-10 21:20:35 +0000

    Completed task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        # With this I take the first value of the dictionary
+        bigger = list(a_dictionary.values())[0]
+        for key in a_dictionary.keys():
+            if a_dictionary[key] > bigger:
+                bigger = a_dictionary[key]
+        return bigger
+    else:
+        return None 
