ee630295132eaeec097f3f724c24c941e662c95d
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-09-10 22:48:36 +0000

    Corrected pep8

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -6,6 +6,6 @@ def weight_average(my_list=[]):
         for tup in my_list:
             sum_score += tup[0] * tup[1]
             total_w += tup[1]
-        return sum_score / total_w 
+        return sum_score / total_w
     else:
         return 0
