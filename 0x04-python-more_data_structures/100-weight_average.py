fb440d019733d40a836cb592365c9ae17214e14c
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-09-10 22:47:36 +0000

    Completed task 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list:
+        sum_score = 0
+        total_w = 0
+        for tup in my_list:
+            sum_score += tup[0] * tup[1]
+            total_w += tup[1]
+        return sum_score / total_w 
+    else:
+        return 0
