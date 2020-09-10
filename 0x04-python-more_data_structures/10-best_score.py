9f575185a2499a1776db1db416da9d7bc6ab8edd
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-09-10 21:32:52 +0000

    Corrected return variable for task 10. I was supposed to return the key and I was returning the value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
+        # With this I take the first key of the dictionary
+        winner = list(a_dictionary.keys())[0]
+
         # With this I take the first value of the dictionary
         bigger = list(a_dictionary.values())[0]
+
         for key in a_dictionary.keys():
             if a_dictionary[key] > bigger:
                 bigger = a_dictionary[key]
-        return bigger
+                winner = key
+        return winner
     else:
         return None
