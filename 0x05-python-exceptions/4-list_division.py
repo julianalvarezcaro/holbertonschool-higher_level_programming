078460136dac9c65830d55fce1beeaaafbb65ff5
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-09-21 20:56:14 +0000

    Corrected task 4 using a finally block

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,17 +3,17 @@ def list_division(my_list_1, my_list_2, list_length):
     pos = 0
     result = []
     while(pos < list_length):
+        div = 0
         try:
-            result.append(my_list_1[pos] / my_list_2[pos])
+            div = my_list_1[pos] / my_list_2[pos]
         except TypeError:
-            result.append(0)
             print("wrong type")
         except ZeroDivisionError:
-            result.append(0)
             print("division by 0")
         except IndexError:
-            result.append(0)
             print("out of range")
             return result
+        finally:
+            result.append(div)
         pos += 1
     return result
