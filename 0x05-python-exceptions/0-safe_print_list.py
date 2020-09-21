81e784df89767984167c16a07cefae0fb5c209d4
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-09-21 19:45:36 +0000

    Accidentally added a semicolon at the end of a line. Corrected.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     pos = 0
     while (pos < x):
         try:
-            print("{}".format(my_list[pos]), end='');
+            print("{}".format(my_list[pos]), end='')
         except IndexError:
             print()
             return pos
