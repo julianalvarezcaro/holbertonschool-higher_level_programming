8be96e5041c243eed99ed36cf1cff65063923968
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-09-22 03:00:59 +0000

    Corrected return in exception Index Error. Maybe it has to fill as many zeros as list_length dictates

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,7 +11,6 @@ def list_division(my_list_1, my_list_2, list_length):
             print("division by 0")
         except IndexError:
             print("out of range")
-            return result
         finally:
             result.append(div)
     return result
