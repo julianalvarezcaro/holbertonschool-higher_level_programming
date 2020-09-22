48f7571f502bd0855ba4c3e2b1116ba9ded3fb7c
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-09-22 17:59:36 +0000

    Completed task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """Class constructor"""
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """Returns the area of the square"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Getter for the attribute 'size'"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Setter for the attribute 'size'"""
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
