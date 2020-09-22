d5b106c5a9071e57513c6063764fdc58407a0a94
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-09-22 18:14:55 +0000

    COmpleted task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """Class constructor"""
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """Returns the area of the square"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Getter for the attribute 'size'"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Setter for the attribute 'size'"""
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def my_print(self):
+        """Prints a square of __size size with caracter #"""
+        if self.__size == 0:
+            print()
+        for fil in range(self.__size):
+            for col in range(self.__size):
+                print("#", end='')
+            print()
