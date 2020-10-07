425045802128955a1206b0f04732f4abd7a870b7
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-10-07 22:49:03 +0000

    I was no validating if the file was containing somwthing or not

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""9-add_item module"""
+"""
+9-add_item module
+Saves the list contaning the args into a file
+"""
 
 import sys
 import json
@@ -7,15 +10,12 @@ import json
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
+args = sys.argv[1:]
 filename = "add_item.json"
 
+prev_content = load_from_json_file(filename)
+if not prev_content:
+    prev_content = []
 
-def main(args):
-    """Saves the list contaning the args into a file"""
-    prev_content = load_from_json_file(filename)
-
-    new_cont = prev_content + args
-    save_to_json_file(new_cont, filename)
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
+new_cont = prev_content + args
+save_to_json_file(new_cont, filename)
