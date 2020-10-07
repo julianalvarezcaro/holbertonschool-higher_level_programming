4812e5395f7bc787c2e339caa2f0c5e59802a190
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-10-07 00:44:40 +0000

    Completed task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""11-square module"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class that inherites from Rectangle"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Returns the area of the square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
