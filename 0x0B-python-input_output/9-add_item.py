e42d607bc3da6c886e924849e412a57e7fd259a5
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-10-07 22:55:54 +0000

    I was trying to read the file even if it didn't exist

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -12,9 +12,9 @@ load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 args = sys.argv[1:]
 filename = "add_item.json"
 
-prev_content = load_from_json_file(filename)
-if not prev_content:
-    prev_content = []
+prev_content = []
+if os.path.exists("./add_item.json"):
+    prev_content = load_from_json_file(filename)
 
 new_cont = prev_content + args
 save_to_json_file(new_cont, filename)
