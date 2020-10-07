118a2367c4c8a7f3e8c991be6eb47ceff22a08ab
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-10-07 22:57:04 +0000

    How could I forget to import the OS module?

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -5,6 +5,7 @@ Saves the list contaning the args into a file
 """
 
 import sys
+import os
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
