484cdce268e140e4e8bdeedf1e05585d5c510e4f
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-11-17 00:57:16 +0000

    KJDHBQWD

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -7,6 +7,3 @@ CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table 
 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-ALTER TABLE first_table
-MODIFY `name` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
