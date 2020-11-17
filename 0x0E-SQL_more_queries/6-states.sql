151877ae59cdf7130b930d081a60ea667ee30fab
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-11-17 18:04:08 +0000

    Completed taks 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- Creates a database and a table
+-- Database: hbtn_0d_usa. Table: states. Fields: id, name
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    `name` VARCHAR(256) NOT NULL
+);
