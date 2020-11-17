da78c35c9ce4b85a24be5fea293232c92d75a3f8
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-11-17 19:27:49 +0000

    Corrected column name. Was using default 'name' intead of the 'genre' name that was requested

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Lists all genres and displays the number of shows linked to each
 -- Display: <TV Show genre> - <Number of shows linked to this genre>
-SELECT tv_genres.name, COUNT(tv_show_genres.show_id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_show_genres
 RIGHT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
