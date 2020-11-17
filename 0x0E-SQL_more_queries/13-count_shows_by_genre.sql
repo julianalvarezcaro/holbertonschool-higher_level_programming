05ad4c65464f84f8649cfa9758f02c479a9d8f68
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-11-17 19:32:32 +0000

    Corrected RIGHT JOIN for INNER JOIN since requierement says that must not show a genre if has no shows linked

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,6 +2,6 @@
 -- Display: <TV Show genre> - <Number of shows linked to this genre>
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_show_genres
-RIGHT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC
