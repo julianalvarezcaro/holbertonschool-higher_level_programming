fff8898f8fc983acf3f393076f711b514e51330c
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-11-17 18:38:27 +0000

    Completed task 8

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- Lists all the cities of California
+-- Results sorted by cities.id
+SELECT id, `name`
+FROM cities
+WHERE state_id = 
+    (SELECT id
+    FROM states
+    WHERE `name` = "California");
