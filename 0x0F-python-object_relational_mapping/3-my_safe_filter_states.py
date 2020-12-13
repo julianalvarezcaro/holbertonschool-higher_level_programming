afb4fc8984eb23ec62ff4b3fd87f5259f6448747
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-12-13 18:37:39 +0000

    Deleted chaset

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -16,12 +16,11 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost',
                          port=3306,
                          user=username,
-                         db=db_name,
-                         charset="utf8")
+                         db=db_name)
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states WHERE name=%s\
-                ORDER BY states.id ASC", (name,))
+                ORDER BY id", (name,))
     rows = cur.fetchall()
     for row in rows:
         print(row)
