e69a28617524d1972554420cd6c938d9e30f3542
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-12-13 19:39:53 +0000

    Completed task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Write a script that lists all State objects from the database hbtn_0e_6_usa
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, db_name))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    quer = session.query(State).first()
+    if quer:
+        print("{}: {}".format(instance.id, instance.name))
+    else:
+        print("Nothing")
+    session.close()
