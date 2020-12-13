55b6a1d74f92b7e38613cfe9914d0709c005b46b
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-12-13 19:36:09 +0000

    Corrected arguments

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-
 """Write a script that lists all State objects from the database hbtn_0e_6_usa
 """
-
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sys import argv
 
-
 if __name__ == "__main__":
-    username = args[1]
-    password = args[2]
-    db_name = args[3]
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(username, password, db_name))
