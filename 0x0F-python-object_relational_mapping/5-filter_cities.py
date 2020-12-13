20b7ebb73bab4ae21995abc579b169ad306b85d2
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-12-13 18:43:53 +0000

    Corrected localhost

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     db_name = args[3]
     name = args[4]
 
-    db = MySQLdb.connect(host='127.0.0.1',
+    db = MySQLdb.connect(host='localhost',
                          port=3306,
                          user=username,
                          passwd=password,
