43e7bd9d95e796ea114d2e2414bd9d2f6aeb6706
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-12-18 18:26:11 +0000

    Completly removed additional comments

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a GET request to the URL
 curl -s -L -G "$1"
-# INITIALY I TRIED: curl -G "0.0.0.0:5000/route_1"
-# But it says that the page has a redirection, so I had to follow that
