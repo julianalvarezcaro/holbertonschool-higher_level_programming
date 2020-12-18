d6a310f9518058a574d39db5f07976a98531f4ba
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-12-18 18:27:33 +0000

    Deleted addional comment lines

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Sends a DELETE request to the URL passed as the first argument
-# and displays the body of the response
-#
-# -X excutes requests. I think I could have used that in the previuouds task
 curl -s -X DELETE "$1"
