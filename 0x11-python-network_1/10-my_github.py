7d3079207f72103755a1648faf13af614d995a8b
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-01-11 23:46:26 +0000

    Completed task 9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Displays my GitHub id
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    user = argv[1]
+    passw = argv[2]
+
+    response = requests.get('https://api.github.com/user', auth=(user, passw))
+    jfile = response.json()
+    print(jfile['id'])
+    
