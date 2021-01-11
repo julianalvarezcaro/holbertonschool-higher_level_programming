565ac660343ec6ac916bc63a39ebd1b6006d44f5
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-01-11 22:46:19 +0000

    Completed task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Sends a POST request
+Displays body of the response
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    import requests
+
+    response = requests.post(argv[1], data = {'email': argv[2]})
+    print(response.text)
