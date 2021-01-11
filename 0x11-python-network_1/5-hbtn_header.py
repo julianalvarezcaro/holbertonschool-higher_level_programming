70517d3dd9fe8fb25e6e3888cddd3452a9abb13f
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-01-11 22:33:41 +0000

    Completed taks 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Sends a request
+Displays the value of the X-Request-Id variable
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    request = requests.get(argv[1])
+    req_id = request.headers['X-Request-Id']
+    print(req_id)
