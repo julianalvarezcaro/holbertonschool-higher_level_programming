f7d93aff0cab55df7f3b68299306cf088197b3a5
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-01-11 21:39:35 +0000

    Completed taks 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Takes in URL
+Displays body of response
+Manages HTTPError exceptions
+"""
+
+if __name__ == "__main__":
+    from urllib.error import HTTPError
+    from urllib import request
+    from sys import argv
+
+    url = argv[1]
+
+    try:
+        req = request.Request(url)
+
+        response = request.urlopen(req)
+        print(response.read().decode())
+    except HTTPError as error:
+        print("Error code: {}".format(error.code))
