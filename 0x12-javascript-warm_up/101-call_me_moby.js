f96aaeeeb80d480252129a2b7a9f4e97194e558c
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-02-03 03:08:47 +0000

    Corrected Semistandard

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-module.exports.callMeMoby = function (x, theFunction){
+module.exports.callMeMoby = function (x, theFunction) {
   let i;
-  for (i = 0; i < x; i++){
+  for (i = 0; i < x; i++) {
     theFunction();
   }
-}
+};
