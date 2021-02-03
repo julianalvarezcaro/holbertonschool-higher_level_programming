9ec9909a22313d1144935d8d187ede3897b929a7
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-02-03 02:04:02 +0000

    Corrected comparing function. It was taking the arguments as string, so when comparing negative numbers the result was the opposite to the expected

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 
-function compareFunc (a, b) {
+function compareFunc (x, y) {
+  const a = parseInt(x);
+  const b = parseInt(y);
   if (a > b) {
     return -1;
   }
@@ -14,6 +16,6 @@ const args = process.argv.slice(2);
 if (args.length <= 1) {
   console.log(1);
 } else {
-  const secondBig = args.sort(compareFunc)[1];
+  const secondBig = args.sort(compareFunc);
   console.log(secondBig);
 }
