9cd9bade32bd10032bca32f23d4d6416996f9641
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-02-09 00:18:42 +0000

    Completed task 2. If width or height are equal or less than 0, the instance will be empty

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const myClass = class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+};
+module.exports = myClass;
