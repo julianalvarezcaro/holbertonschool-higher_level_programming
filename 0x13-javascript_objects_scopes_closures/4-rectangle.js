698a1f9b4792bdcae54f19f0ea3549452089b166
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-02-09 00:44:42 +0000

    Completed task 4. Instace method that rotates the rectangle, and another that doubles its size

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+const myClass = class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    let h;
+    let w;
+    for (h = 0; h < this.height; h++) {
+      for (w = 0; w < this.width; w++) {
+        process.stdout.write('X');
+      }
+      console.log();
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
+module.exports = myClass;
