ec960fc71d08f16b8f59d1707ea49add8c7b942e
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-02-09 02:08:16 +0000

    Completed task 8

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  const lastIndex = list.length - 1;
+  let index;
+  let reversed = [];
+
+  for (index = lastIndex; index >= 0; index--) {
+    reversed.push(list[index]);
+  }
+  return reversed
+}
